70476c01d05f3c88888f673afdd85fe0b0a836ed
daniel <3091549027@qq.com>
2019-11-13 18:29:23 +0800

    把本地工程添加到git中

---
+++ git-test-project/src/cn/itcast/ccf/gitDemo1.java
@@ -0,0 +1,11 @@
+package cn.itcast.ccf;
+
+public class gitDemo1 {
+    public static void main(String[] args) {
+        System.out.println("git第1次！！！");
+        System.out.println("git第2次！！！");
+        System.out.println("git第3次！！！");
+        System.out.println("git第4次！！！");
+        System.out.println("git第5次！！！");
+    }
+}
